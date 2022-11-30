abe41355da5598b25653b946d0710f3bbd790366
Sergey Kosichkin <d978827@icloud.com>
2022-11-30 21:10:49 +0300

    Added logic for the button.

--- TrafficLightAppNew/ViewController.swift
+++ TrafficLightAppNew/ViewController.swift
@@ -9,11 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var changeButton: UIButton!
+    
+    private let lightOff: CGFloat = 0.3
+    private let lightOn: CGFloat = 1
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        changeButton.layer.cornerRadius = 15
+        
+        redView.alpha = lightOff
+        yellowView.alpha = lightOff
+        greenView.alpha = lightOff
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        let cornerRadius = redView.bounds.height * 0.5
+        
+        redView.layer.cornerRadius = cornerRadius
+        yellowView.layer.cornerRadius = cornerRadius
+        greenView.layer.cornerRadius = cornerRadius
+        
     }
 
-
+    @IBAction func buttonPressed() {
+        
+        if greenView.alpha == lightOn {
+            redView.alpha = lightOn
+            greenView.alpha = lightOff
+        } else if redView.alpha == lightOn {
+            yellowView.alpha = lightOn
+            redView.alpha = lightOff
+        } else if yellowView.alpha == lightOn {
+            greenView.alpha = lightOn
+            yellowView.alpha = lightOff
+        } else {
+            redView.alpha = lightOn
+            changeButton.setTitle("NEXT", for: .normal)
+        }
+        
+    }
+    
+    
 }
 
